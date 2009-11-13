ce93806494ee36bb256e522c605ecbf72e3aea04
Tim Freeman <tfreeman@mcs.anl.gov>
2009-11-13 15:43:22 -0600

    compositio wordpress theme, free courtesy of smashingmagazine.com and Elena Gafita

---
+++ footer.php
@@ -0,0 +1,16 @@
+</div></div>
+<?php get_sidebar(); ?>
+<!-- Container -->
+
+
+</div></div>
+<!-- End BG -->
+
+<div class="footer">
+<p class="copy">Copyright &copy; 2009 - <? bloginfo('name'); ?></p> 
+<p class="theme">Compositio Theme is created by: <a href="http://designdisease.com/" title="Professional Blog Design">Design Disease</a> brought to you by <a href="http://premiumthemes.com/">PremiumThemes.com</a></p> 
+</div>
+
+
+</body>
+</html>
