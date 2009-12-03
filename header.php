becc2625da6803a6eaf4f85f4598fdf6b388cdee
Tim Freeman <tim@timfreeman.org>
2009-12-02 23:11:44 -0600

    credit if someone views source

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 
+<!-- The scienceclouds.org wordpress theme is an alteration of the original "Redtime" design by WebDesignLessons.com: http://www.webdesignlessons.com/redtime-wordpress-theme/ -->
+
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
