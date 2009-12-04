b1306570763f6654bd54311b3c96380f269c514f
Tim Freeman <tim@timfreeman.org>
2009-12-04 09:37:42 -0600

    marketplace looking acceptable

--- marketplace.php
+++ marketplace.php
@@ -9,7 +9,43 @@ Template Name: Marketplace
     <div id="toc_left">
         <div id="toc_left_content">
         
-            hello ONE
+        <a name="toc"></a>
+
+        <h2>Table of Contents</h2>   
+        
+        <ul>
+            <li>
+            <a href="#workspaces">Images:</a>
+                <ul>
+                    <li><a href="#hellocloud">Hello Cloud</a></li>
+                    <li><a href="#globus002">Globus 002</a></li>
+                    <li><a href="#base-cluster-01">Base Cluster cc12</a></li>
+                    <li><a href="#osg-cluster-1.0.0">OSG Cluster 1.0.0</a></li>
+                    <li><a href="#hadoop-cluster-01">Hadoop Cluster 01</a></li>
+                    <li><a href="#blast-cluster-02">BLAST Cluster 02</a></li>
+                    <li><a href="#test-vm">Workspace test VM</a></li>
+                    <li><a href="#cernvm">CernVM</a></li>
+                </ul>
+            </li>
+            <li><a href="#resources">Resources</a>
+                <ul>
+                    <li>
+                        <a href="http://workspace.globus.org/vm/marketplace-notes.html">Extended notes page</a>
+                    </li>
+                    <li>
+                        <a href="http://workspace.globus.org/vm/TP2.2/doc/metadata-quickstart.html">Metadata Quickstart</a>
+                    </li>
+                    <li>
+                        <a href="http://workspace.globus.org/vm/offline-editing.html">Offline Editing Guide</a>
+                    </li>
+                    <li>
+                        <a href="http://workspace.globus.org/vm/module-copying.html">Module Copying Guide</a>
+                    </li>
+                </ul>
+            </li>
+            <li><a href="#other">Other VMs online</a>
+            </li>
+        </ul>
         </div>
     </div>
 
